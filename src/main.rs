74978b4b08b1d384bb3b1a4d1178acc52d98047d
Dan Schwabe <danschwabe@protonmail.com>
2020-03-26 16:13:38 -0500

    convert to simple socket server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod shapes;
 mod formatter;
+use std::net::{TcpListener};
+use std::io::Write;
 extern crate rand;
 
+
 fn main() {
+    let listener = TcpListener::bind("127.0.0.1:55555").unwrap();
+    let mut socket = listener.accept().unwrap();
+
     let shape = shapes::Square {
         x: rand::random::<u8>()%20,
         y: rand::random::<u8>()%20,
     };
-
     let formatter = formatter::Formatter{ shape, };
+    let result = formatter.format();
 
-    let format = formatter.format();
-
-    println!("{}", format);
+    socket.0.write(result.as_bytes()).unwrap();
 }
